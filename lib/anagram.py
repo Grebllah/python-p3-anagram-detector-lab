b7dcd618a9f4d6dd597cbeee9e6fd08e234dda94
James Hallberg <Grebllah81788@gmail.com>
2023-12-18 17:08:49 -0500

    anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, chosen_word):
+        self.chosen_word = chosen_word
+    def match(self, matches):
+        anagrams = []
+        # loop through the matches, a list of strings, then for each string, make a sorted list of all the letters. compare this to the sorted list of the chosen words letters.
+        for match in matches:
+            match_list = [*match]
+            if sorted(match_list) == sorted([*self.chosen_word]):
+                anagrams.append(match)
+        return anagrams
